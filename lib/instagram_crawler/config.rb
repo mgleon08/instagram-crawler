0c3525a0df4e95a84bf05dd96a6864e42b60654b
Leon Ji <mgleon08@gmail.com>
2018-11-22 13:50:07 +0800

    Add config

---
+++ lib/instagram_crawler/config.rb
@@ -0,0 +1,22 @@
+module InstagramCrawler
+  class Config
+    @default_url = "https://www.instagram.com".freeze
+    class << self
+      attr_reader :default_url, :user_name, :base_url, :base_path,
+                  :log_path, :after_date, :parse_date
+      attr_accessor :download
+
+      def user_name=(user_name)
+        @user_name = user_name
+        @base_url  = "#{default_url}/#{user_name}/"
+        @base_path = "./instagram_crawler/#{user_name}"
+        @log_path = "./instagram_crawler/#{user_name}/log_file"
+      end
+
+      def after_date=(after_date)
+        @after_date = after_date
+        @parse_date = Time.parse(after_date).to_i
+      end
+    end
+  end
+end
