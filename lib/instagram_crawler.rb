433b94f66f7ef582f2c10da9cb1eae488a92e067
Leon Ji <mgleon08@gmail.com>
2018-11-22 13:51:23 +0800

    Add shell command

--- lib/instagram_crawler.rb
+++ lib/instagram_crawler.rb
@@ -1,6 +1,26 @@
+$LOAD_PATH << File.dirname(__FILE__)
+
+require "optparse"
+require "http"
+require "nokogiri"
+require "logger"
+require "colorize"
+
 require "instagram_crawler/version"
 
-module InstagramCrawler
-  class Error < StandardError; end
-  # Your code goes here...
-end
+require "instagram_crawler/main"
+require "instagram_crawler/file"
+require "instagram_crawler/logger"
+require "instagram_crawler/multi_io"
+require "instagram_crawler/config"
+
+# parser
+require "instagram_crawler/parser/args"
+require "instagram_crawler/parser/base"
+require "instagram_crawler/parser/html"
+require "instagram_crawler/parser/json"
+
+# error
+require "instagram_crawler/errors/http_error"
+require "instagram_crawler/errors/env_error"
+require "instagram_crawler/errors/arg_error"
