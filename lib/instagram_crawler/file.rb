4e584411d58192a83f283ee5497ccac17295f3b5
Leon Ji <mgleon08@gmail.com>
2019-03-27 10:34:34 +0800

    Fix wrong extname

--- lib/instagram_crawler/file.rb
+++ lib/instagram_crawler/file.rb
@@ -9,13 +9,19 @@ module InstagramCrawler
 
       def download(url, dir_name, file_name)
         return unless Config.download
-        extname = File.extname(url)
+        binary_data, mime_type = get_binary_data(url)
+
+        extname =
+          case mime_type
+          when "video/mp4" then ".mp4"
+          when "image/jpeg" then ".jpeg"
+          end
 
         dir_path  = "#{Config.base_path}/#{dir_name}"
         FileUtils.mkdir_p(dir_path) unless Dir.exist?(dir_path)
 
         file_path = "#{dir_path}/#{file_name}#{extname}"
-        binary_data = get_binary_data(url)
+
 
         File.open(file_path, 'wb') do |f|
           f.write binary_data
@@ -28,7 +34,7 @@ module InstagramCrawler
         res = Config.proxyname ?
           HTTP.via(Config.proxyname, Config.port).get(url) : HTTP.get(url)
         raise Errors::HttpError, "#{res.code} #{res.reason}" if res.code != 200
-        res.to_s
+        [res.to_s, res.mime_type]
       end
     end
   end
